2434997ece4684e82880a92ace88dc439adeda16
Yanir Elfassy <yanir.elfassy@mail.huji.ac.il>
2021-06-05 22:07:19 +0300

    Place order UI

--- app/src/main/java/exercise/android/sandwich/PlaceOrder.java
+++ app/src/main/java/exercise/android/sandwich/PlaceOrder.java
@@ -2,19 +2,21 @@ package exercise.android.sandwich;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.NumberPicker;
 
-public class MainActivity extends AppCompatActivity {
+public class PlaceOrder extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, PlaceOrder.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_place_order);
+
+        NumberPicker np = (NumberPicker) findViewById(R.id.picklesPicker);
+        np.setMinValue(0);
+        np.setMaxValue(10);
     }
 }
