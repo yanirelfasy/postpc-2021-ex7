51ed78aaf59682a1328039b3d971d6be12cdc898
Yanir Elfassy <yanir.elfassy@mail.huji.ac.il>
2021-06-25 15:57:07 +0300

    Place order screen save state and values

---
+++ app/src/main/java/exercise/android/sandwich/EditOrder.java
@@ -0,0 +1,14 @@
+package exercise.android.sandwich;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_order);
+    }
+}
--- app/src/main/java/exercise/android/sandwich/PlaceOrder.java
+++ app/src/main/java/exercise/android/sandwich/PlaceOrder.java
@@ -1,13 +1,24 @@
 package exercise.android.sandwich;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.NumberPicker;
 
-public class PlaceOrder extends AppCompatActivity {
+public class PlaceOrder extends AppCompatActivity{
+    private String fullName;
+    private int numOfPickles;
+    private boolean addHummus;
+    private boolean addTahini;
+    private String comment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +26,93 @@ public class PlaceOrder extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_place_order);
 
-        NumberPicker np = (NumberPicker) findViewById(R.id.picklesPicker);
-        np.setMinValue(0);
-        np.setMaxValue(10);
+        EditText fullNameField = (EditText) findViewById(R.id.name);
+        NumberPicker numOfPicklesField = (NumberPicker) findViewById(R.id.picklesPicker);
+        CheckBox addHummusField = (CheckBox) findViewById(R.id.hummusCheckBox);
+        CheckBox addTahiniField = (CheckBox) findViewById(R.id.tahiniCheckBox);
+        EditText commentField = (EditText) findViewById(R.id.commentContent);
+
+        numOfPicklesField.setMinValue(0);
+        numOfPicklesField.setMaxValue(10);
+
+        numOfPicklesField.setOnValueChangedListener((picker, oldVal, newVal) -> {
+            this.numOfPickles = newVal;
+        });
+
+        fullNameField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                fullName = fullNameField.getText().toString();
+            }
+        });
+
+        commentField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                comment = commentField.getText().toString();
+            }
+        });
+
+
+        addHummusField.setOnCheckedChangeListener((compoundButton, isChecked) -> addHummus = isChecked);
+        addTahiniField.setOnCheckedChangeListener(((compoundButton, isChecked) -> addTahini = isChecked));
+
+
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("fullName", fullName);
+        outState.putInt("numOfPickles", numOfPickles);
+        outState.putBoolean("addHummus", addHummus);
+        outState.putBoolean("addTahini", addTahini);
+        outState.putString("comment", comment);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        fullName = savedInstanceState.getString("fullName");
+        numOfPickles = savedInstanceState.getInt("numOfPickles");
+        addHummus = savedInstanceState.getBoolean("addHummus");
+        addTahini = savedInstanceState.getBoolean("addTahini");
+        comment = savedInstanceState.getString("comment");
+
+
+        EditText fullNameField = (EditText) findViewById(R.id.name);
+        NumberPicker numOfPicklesField = (NumberPicker) findViewById(R.id.picklesPicker);
+        CheckBox addHummusField = (CheckBox) findViewById(R.id.hummusCheckBox);
+        CheckBox addTahiniField = (CheckBox) findViewById(R.id.tahiniCheckBox);
+        EditText commentField = (EditText) findViewById(R.id.commentContent);
+
+
+        fullNameField.setText(fullName);
+        numOfPicklesField.setValue(numOfPickles);
+        addHummusField.setChecked(addHummus);
+        addTahiniField.setChecked(addTahini);
+        commentField.setText(comment);
     }
 }
+
